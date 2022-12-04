fbcda26630bf000eb98f62853ca44416b20aec5b
Tessa Grainger <tessa.robyn@outlook.com>
2022-12-04 18:27:59 +0000

    Tested arraylib

--- arrayLib.js
+++ arrayLib.js
@@ -1,4 +1,4 @@
-class Array {
+export class Array {
   constructor(size) {
     this.size = size;
     this.array = [];
---
+++ orderingScript.js
@@ -0,0 +1,6 @@
+import { Array } from "./arrayLib.js";
+
+const array = new Array(10);
+array.assign(9, 2);
+console.log(array);
+array.assign(10, 20);
